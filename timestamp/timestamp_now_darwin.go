c4b2e163255d3250787d0d2c9c5bc8121cc6c6a8
wangfeng <wangfengxy@sina.cn>
2025-09-19 11:14:42 +0800

    删除废弃的不同平台的时间戳实现

---
+++ timestamp/timestamp_now_darwin.go
@@ -0,0 +1,12 @@
+//go:build darwin
+
+package timestamp
+
+import (
+	_ "unsafe" // for go:linkname
+)
+
+//go:linkname walltime runtime.walltime
+func walltime() (int64, int32)
+
+// 此文件已不再需要，现在直接使用now()函数
---
+++ timestamp/timestamp_now_other.go
@@ -0,0 +1,5 @@
+//go:build !darwin
+
+package timestamp
+
+// 此文件已不再需要，现在直接使用now()函数
