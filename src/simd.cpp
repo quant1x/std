5aeac8f30d3c55635be513356f9fae93eb7e7d8e
wangfeng <wangfengxy@sina.cn>
2025-09-20 08:45:46 +0800

    补充simd流程注释

--- src/simd.cpp
+++ src/simd.cpp
@@ -7,6 +7,7 @@ void sample_mean(const vector_type& a, const vector_type& b, vector_type& res)
     constexpr std::size_t simd_size = xsimd::simd_type<double>::size;
     std::size_t vec_size = size - size % simd_size;
 
+    // SIMD vectorized loop
     for(std::size_t i = 0; i < vec_size; i += simd_size)
     {
         auto ba = xsimd::load_aligned(&a[i]);
@@ -14,6 +15,8 @@ void sample_mean(const vector_type& a, const vector_type& b, vector_type& res)
         auto bres = (ba + bb) / 2.;
         bres.store_aligned(&res[i]);
     }
+    
+    // Scalar fallback for remainder
     for(std::size_t i = vec_size; i < size; ++i)
     {
         res[i] = (a[i] + b[i]) / 2.;
